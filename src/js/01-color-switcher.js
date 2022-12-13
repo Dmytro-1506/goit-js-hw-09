513ebf5d7e351ef8cbaad541d2f80d38261c3ae1
Dmytro-1506 <kyunlik@gmail.com>
2022-12-13 22:15:28 +0100

    switch color complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,21 @@
-import throttle from "lodash.throttle";
-
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let changeColor = null;
+
+startBtn.addEventListener('click', () => {
+  changeColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+})
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled', 'disabled');
+  clearInterval(changeColor);
+})
