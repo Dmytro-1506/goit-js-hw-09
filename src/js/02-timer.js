405b48f92b2d86edf9862bd053246f690198dd3d
Dmytro-1506 <kyunlik@gmail.com>
2022-12-14 21:00:54 +0100

    timer complete

---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const button = document.querySelector('button[data-start]');
+const input = document.querySelector('#datetime-picker');
+const daysOnPage = document.querySelector('span[data-days]');
+const hoursOnPage = document.querySelector('span[data-hours]');
+const minutesOnPage = document.querySelector('span[data-minutes]');
+const secondsOnPage = document.querySelector('span[data-seconds]');
+
+let nowTime = 0;
+let timerTime = 0;
+let convertedTime = {};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    convertedTime = { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
+}
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        let selectedTime = selectedDates[0].getTime();
+        if (nowTime <= selectedTime) {
+            button.removeAttribute('disabled');
+            timerTime = selectedTime - nowTime;
+            convertMs(timerTime);
+            return 
+        }
+        button.setAttribute('disabled', 'disabled');
+        window.alert("Please choose a date in the future");
+    },
+};
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function changeTimeOnPage() {
+    if (timerTime > 0) {
+        daysOnPage.textContent = pad(convertedTime.days);
+        hoursOnPage.textContent = pad(convertedTime.hours);
+        minutesOnPage.textContent = pad(convertedTime.minutes);
+        secondsOnPage.textContent = pad(convertedTime.seconds);
+        timerTime -= 1000;
+        convertMs(timerTime);
+        console.log('hello');
+    }
+    clearInterval(onClick);
+}
+
+const onClick = () => { setInterval(changeTimeOnPage, 1000); }
+
+button.addEventListener('click', onClick);
+
+input.addEventListener('focus', () => {
+    flatpickr(input, options);
+    nowTime = options.defaultDate.getTime();
+})
