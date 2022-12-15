700b0d8caf88b26d7faf37f19a6fc1fef2169f75
Dmytro-1506 <kyunlik@gmail.com>
2022-12-15 18:50:39 +0100

    promise complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,64 @@
+import Notiflix from 'notiflix';
+
+const firstDelay = document.querySelector('.prom-delay');
+const delayStep = document.querySelector('.prom-step');
+const amount = document.querySelector('.prom-amount');
+const form = document.querySelector('.form');
+
+let positionNum = 1;
+let delayNum = 0;
+
+let firstDelayNum = 0;
+firstDelay.addEventListener('input', () => {
+  firstDelayNum = Number(firstDelay.value);
+  delayNum = Number(firstDelay.value);
+})
+
+let delayStepNum = 0;
+delayStep.addEventListener('input', () => {
+  delayStepNum = Number(delayStep.value);
+})
+
+let amountNum = 0;
+amount.addEventListener('input', () => {
+  amountNum = Number(amount.value);
+  console.log(amountNum);
+})
+
+function counter(interval, quantity) {
+  if (quantity > 1) {
+    amountNum -= 1;
+    positionNum += 1;
+    delayNum += interval;
+    createPromise(positionNum, delayNum);
+    return
+  }
+  clearInterval(intervalId);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  const shouldResolve = Math.random() > 0.5;
+
+    if (shouldResolve) {
     // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
-  }
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
 }
+
+let intervalId = null;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  setTimeout(() => {
+    console.log('this is timeout');
+    createPromise(positionNum, firstDelayNum);
+    intervalId = setInterval(() => {
+      console.log('this is interval', delayStepNum);
+        counter(delayStepNum, amountNum);
+      }, delayStepNum);
+  } , firstDelayNum);
+})
