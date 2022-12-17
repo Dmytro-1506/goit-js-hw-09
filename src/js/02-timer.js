40aed8898e024bb25fcd0ce01e7e7bcd06c28de4
Dmytro-1506 <kyunlik@gmail.com>
2022-12-17 19:46:16 +0100

    timer changed

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,9 +9,37 @@ const hoursOnPage = document.querySelector('span[data-hours]');
 const minutesOnPage = document.querySelector('span[data-minutes]');
 const secondsOnPage = document.querySelector('span[data-seconds]');
 
-let nowTime = 0;
 let timerTime = 0;
 let convertedTime = {};
+let nowTime = null;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+  onClose(selectedDates) {
+    let selectedTime = selectedDates[0].getTime();
+    if (nowTime <= selectedTime) {
+      button.removeAttribute('disabled');
+      timerTime = selectedTime - nowTime;
+      convertMs(timerTime);
+    } else {
+      button.setAttribute('disabled', 'disabled');
+      Notiflix.Report.warning('WARNING', 'Please choose a date in the future', 'Try again');
+    }
+  }
+};
+
+const onClick = () => {
+  setInterval(changeTimeOnPage, 1000);
+  button.setAttribute('disabled', 'disabled');
+}
+
+input.addEventListener('focus', () => {
+  flatpickr(input, options);
+  nowTime = options.defaultDate.getTime();
+})
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -32,24 +60,6 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
 }
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        let selectedTime = selectedDates[0].getTime();
-        if (nowTime <= selectedTime) {
-            button.removeAttribute('disabled');
-            timerTime = selectedTime - nowTime;
-            convertMs(timerTime);
-            return 
-        }
-        button.setAttribute('disabled', 'disabled');
-      Notiflix.Report.warning('WARNING', 'Please choose a date in the future', 'Try again');
-    },
-};
-
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 }
@@ -66,11 +76,4 @@ function changeTimeOnPage() {
     clearInterval(onClick);
 }
 
-const onClick = () => { setInterval(changeTimeOnPage, 1000); }
-
 button.addEventListener('click', onClick);
-
-input.addEventListener('focus', () => {
-    flatpickr(input, options);
-    nowTime = options.defaultDate.getTime();
-})
